4dd8a6c89439ff8725822058ff923b68a657c72f
Maxym Rusnachenko <88180470+r0zymnuk@users.noreply.github.com>
2022-10-26 20:22:34 +0300

    lab 3 4

--- scripts.js
+++ scripts.js
@@ -1,18 +1,11 @@
-let img_urls =['https://www.wsupercars.com/wallpapers-regular/Mercedes-Benz/2022-Mercedes-AMG-SL63-008-1080.jpg', 
-'https://www.wsupercars.com/wallpapers-regular/Mercedes-Benz/2023-Mercedes-AMG-C63-S-E-Performance-009-1080.jpg',
-'https://www.wsupercars.com/wallpapers-regular/Mercedes-Benz/2019-Mercedes-AMG-G63-005-1080.jpg',
-'https://www.wsupercars.com/wallpapers-regular/Mercedes-Benz/2023-Mercedes-AMG-EQE-53-002-1080.jpg',
-'https://wallpapercave.com/wp/wp7951604.jpg']
-
-let img_carousel_el = document.getElementById("head-carousel")
-var i = 0
-
-function changeImageURL(){
-    img_carousel_el.src = img_urls[i]
-    i++
-    if(i >= 4){
-        i = 0
+var myNav = document.getElementById('nav');
+window.onscroll = function () { 
+    if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 200 ) {
+        myNav.classList.add("nav-scrolled");
+        myNav.classList.remove("nav-top");
+    } 
+    else {
+        myNav.classList.add("nav-top");
+        myNav.classList.remove("nav-scrolled");
     }
-}
-
-const intervalID = setInterval(changeImageURL, 4000)
+};
