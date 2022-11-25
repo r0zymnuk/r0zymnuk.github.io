6d7613d7b22d84c47fa74a121034b31be33dd26b
Maxym Rusnachenko <88180470+r0zymnuk@users.noreply.github.com>
2022-11-25 22:45:54 +0200

    js
    bg color randomizer

--- scripts.js
+++ scripts.js
@@ -1,11 +1,40 @@
-var myNav = document.getElementById('nav');
-window.onscroll = function () { 
-    if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 200 ) {
-        myNav.classList.add("nav-scrolled");
-        myNav.classList.remove("nav-top");
-    } 
-    else {
-        myNav.classList.add("nav-top");
-        myNav.classList.remove("nav-scrolled");
-    }
-};
+const body = document.getElementsByTagName('body')[0]
+
+const nav = document.getElementById('nav');
+
+const scrollProgressBar = document.getElementById('page-scroll-progress-bar')
+
+const scrollProgressMax = document.body.scrollHeight - document.documentElement.clientHeight
+
+var scrollBefore = 0
+var progress = 0
+
+window.addEventListener("scroll", (event) => {
+    var scroll = this.scrollY;
+
+    progress = scroll / scrollProgressMax
+
+    scrollProgressBar.style.width = (progress * 100) + '%'
+
+    scroll <= 10 ? nav.classList.add('nav-top') : nav.classList.remove('nav-top')
+
+    // console.log(scrollBefore + '    ->  ' + scroll)
+    scrollBefore = scroll
+
+});
+
+let bgRandButtons = document.getElementsByClassName('bg-rand-b')
+
+document.body.style.backgroundColor = "white";
+
+var rgb = [255,255,255]
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
+
+function changeBodyBg(button_n){
+    rgb[button_n] = getRandomInt(255)
+    document.body.style.backgroundColor = 'rgb(' + rgb.join(', ') + ')';
+    console.log()
+}
